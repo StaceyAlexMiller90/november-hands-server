3063be99cd5427c3f08fa02f94a5a68ab790a52c
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-07-17 14:51:33 +0200

    Adjusting product model

--- models/product.js
+++ models/product.js
@@ -6,52 +6,13 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false
-      },
-      colorName: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false
-      },
-      discountId: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      },
-      active: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false
-      },
-      stockQuantity: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      categoryId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      collectionId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
       }
     },
     {}
   )
   product.associate = function (models) {
-    product.belongsTo(models.category)
-    product.belongsTo(models.collection)
-    product.hasOne(models.discount)
-    product.belongsToMany(models.order, {
-      through: 'orderItems',
-      foreignKey: 'productId'
-    })
-    product.hasMany(models.image, {
-      through: 'productImage',
-      foreignKey: 'productId'
-    })
     product.hasMany(models.color, {
-      through: 'productColor',
+      through: 'productColors',
       foreignKey: 'productId'
     })
   }
